dbd0cc865443c824c38161576e2a18249835259b
yeganeh nemati <yeganeh.n666@gmail.com>
2020-10-08 14:22:43 +0330

    add environment configuration

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
-	"github.com/rs/cors"
+	"golang-user-microservice/env"
 	"log"
 	"net/http"
+	"os"
 	"v1/core"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
-var defaultPort = ":8080"
+var bindAddress = env.String("BIND_ADDRESS", false, ":8080", "Bind address for the SSV verify server")
 
 func main() {
+	err := env.Parse()
+	if err != nil {
+		log.Println("Error parsing env", "error", err)
+		os.Exit(1)
+	}
 	cor := cors.New(cors.Options{
 		AllowedOrigins: []string{"*"},
 		AllowedHeaders: []string{"Access-Control-Allow-Origin", "Content-Type", "Session-Key", "Device-ID"},
@@ -20,7 +28,7 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/ping", core.OnPing).Methods("GET")
 
-	port := defaultPort
+	port := *bindAddress
 	handler := cor.Handler(router)
 	http.ListenAndServe(port, handler)
 	log.Println("Listening!")
